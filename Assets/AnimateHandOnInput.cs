30a61c890ed67537f83a0a06d05c6c72e8a0e7d6
Nsreen Alfaifi <116817889+iNSREEN@users.noreply.github.com>
2024-02-24 19:33:33 +0300

    update

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class AnimateHandOnInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty graipAnimationAction;
+    public Animator animator;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float triggierValu = pinchAnimationAction.action.ReadValue<float>();
+        float gripierValu = graipAnimationAction.action.ReadValue<float>();
+
+
+
+        animator.SetFloat("Trigger", triggierValu);
+        animator.SetFloat("Grip", gripierValu);
+
     }
 }
