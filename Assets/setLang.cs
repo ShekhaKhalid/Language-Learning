aecec4bb536417a785028972b2c3fb32f7ba16a4
Nsreen Alfaifi <116817889+iNSREEN@users.noreply.github.com>
2024-02-28 00:10:46 +0300

    update

---
+++ Assets/setLang.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SelectLanguage : MonoBehaviour
+{
+    public TMP_Dropdown dropdown;
+
+    void Start()
+    {
+        dropdown.onValueChanged.AddListener(delegate {
+            OnDropdownValueChanged(dropdown);
+        });
+    }
+
+    void OnDropdownValueChanged(TMP_Dropdown dropdown)
+    {
+        string selectedLanguage = dropdown.options[dropdown.value].text;
+        PlayerPrefs.SetString("selectedLanguage", selectedLanguage);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("AItest");
+    }
+}
