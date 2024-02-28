de54ee1780bc16df580711b76488ff201b2e3580
Lujain4l <147071685+Lujain4l@users.noreply.github.com>
2024-02-28 22:14:51 +0300

    AIConviCoffee

---
+++ Assets/Scripts/UI-Scripts/changeScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeScene : MonoBehaviour
+{
+    [SerializeField] private GameObject info;
+    [SerializeField] private GameObject lang;
+    public void LoadHomepage()
+    {
+        SceneManager.LoadScene("Homepage");
+    }
+
+    public void ShowLang()
+    {
+        info.SetActive(false);
+        info.SetActive(true);
+
+    }
+
+    public void ShowInfo()
+    {
+        info.SetActive(true);
+        info.SetActive(false);
+    }
+}
